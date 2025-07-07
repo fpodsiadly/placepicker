e94d596cac98c85abb3b232d5358c699fb78aca0
Filip <fpodsiadly@gmail.com>
2025-07-07 11:41:10 +0200

    enhance timer management in DeleteConfirmation component

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,19 @@
+import { use } from 'react'
+import { useEffect } from 'react'
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    console.log('Timer set')
+    const timer = setTimeout(() => {
+      onConfirm()
+    }, 3000)
+
+    return () => {
+      console.log('Timer cleared')
+      clearTimeout(timer)
+    }
+  }, [])
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -12,5 +27,5 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
         </button>
       </div>
     </div>
-  );
+  )
 }
